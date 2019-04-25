07cee32cc7e52ab3762414ef10a01c9ebd2dd6e1
wynnja <wynnbsmith98@gmail.com>
2019-04-24 21:56:44 -0400

    Got rid of occType enum. Using an integer instead

--- src/frontend/components/GameCell/index.js
+++ src/frontend/components/GameCell/index.js
@@ -12,19 +12,28 @@ import { Button } from 'semantic-ui-react';
 
 }*/
 
-const occType {
+/*const occType {
   NONE: 1,
   ITEM: 2,
   MONSTER: 3,
   PLAYER: 4
-};
+};*/
+
+/*************occupiedBy****************/
+// I don't know how enums work in JS so I think it would
+// be better to use an int
+//  0 = NOT OCCUPIED
+//  1 = ITEM
+//  2 = MONSTER
+//  3 = PLAYER
+//  4 = other or error? Haven't decided yet.
 
 export default class Cell extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      occupiedBy: occType.NONE,
+      occupiedBy: 0,
       cellNum: this.props.cellNum
     }
   }
