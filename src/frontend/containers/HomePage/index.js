77efffa758f616d470295e58240684f7c1d68fed
ZhunCn <randyzhunc@gmail.com>
2019-04-25 23:53:10 -0400

    Add Player to Database

--- src/frontend/containers/HomePage/index.js
+++ src/frontend/containers/HomePage/index.js
@@ -39,13 +39,13 @@ export default class Home extends React.Component {
   }
 
   componentDidMount() {
-      this.authChecker = firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          console.log(user);
-        } else {
-          console.log('not logged in');
-        }
-      });
+    this.authChecker = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(user);
+      } else {
+        console.log('not logged in');
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -54,80 +54,117 @@ export default class Home extends React.Component {
 
   loginProcedure = () => {
     let self = this;
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    let fb = firebase;
+    let cs = console;
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      cs.log('I\'m in');
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       localStorage.setItem('accountToken', token);
       // The signed-in user info.
       var user = result.user;
       localStorage.setItem('user', user);
+      let uid = user.uid;
+      let wins = 0;
+      let kills = 0;
+      let email = user.email;
+      let name = user.displayName;
+      let location = 'PlayerInfo/listOfPlayers/' + uid;
 
-      self.props.history.push("/");
+      // Insert user as player in database if not in already
+      fb.database().ref('PlayerInfo/listOfPlayers/').once("value", snap => {
+        cs.log('hasChild: ', snap.hasChild(uid));
+        if (!snap.hasChild(uid)) {
+          fb.database().ref(location).set({
+            name,
+            email,
+            wins,
+            kills
+          }).then((data) => {
+            //success callback
+            console.log('data ', data);
+            self.props.history.push("/");
+
+          }).catch((error) => {
+            //error callback
+            console.log('error ', error);
+            self.props.history.push("/");
+          });
+        } else {
+          self.props.history.push("/");
+
+        }
+      });
+
+
+
+    
+/*
+      const rootRef = firebase.database().ref().child('sneaky-strikers');
+      const listOfPlayersRef = rootRef.child('PlayerInfo').child('listOfPlayers');
+      const playerRef = listOfPlayersRef.child(user.uid);
+
+      playerRef.
+      playerRef.on('value', snap => {
+          hasPlayer = snap.val();
+          console.log('hasPlayer has changed!');
+          console.log(hasPlayer);
+
+      });
+      console.log('hasPlayer is: ');
+      console.log(hasPlayer);
+*/
       // ...
     }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-  }
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
 
-  logoutProcedure = () => {
-    firebase.auth().signOut();
-    localStorage.removeItem('accountToken');
-    this.setState({
-      showLogoutModal: false
-    });
-    this.props.history.push("/");
-  }
+logoutProcedure = () => {
+  firebase.auth().signOut();
+  localStorage.removeItem('accountToken');
+  this.setState({
+    showLogoutModal: false
+  });
+  this.props.history.push("/");
+}
 
-  render() {
-    if (hasAccountToken()) {
-      return (
-        <div class="homePage">
-          <div class="Content">
-            <center>
-              <h2>Sneaky Strikers</h2>
-              <Link to="/game"><Button id="startButton"><Icon name='game' />Start a game</Button></Link>
-              <Link to="/leaderboard"><Button id="leaderboardButton"><Icon name='chess king' />Leaderboard</Button></Link>
-              <Button onClick={this.openModal}><Icon name='log out' />Log out</Button>
-
-              <Modal
-                open={this.state.showLogoutModal}
-                onClose={this.closeModal}
-                closeIcon
-              >
-                <Modal.Header>Log out</Modal.Header>
-                <Modal.Content>
-                  <p>Are you sure you want to log out?</p>
-                </Modal.Content>
-                <Modal.Actions>
-                  <Button color='green' onClick={this.logoutProcedure}>
-                    <Icon name='checkmark' /> Yes
-                </Button>
-                  <Button color='red' onClick={this.closeModal}>
-                    <Icon name='remove' /> No, do not log out
-                </Button>
-                </Modal.Actions>
-              </Modal>
-            </center>
-            <div class="Flex">
-          </div>
-          </div>
-        </div>
-      );
-    }
+render() {
+  if (hasAccountToken()) {
     return (
       <div class="homePage">
         <div class="Content">
           <center>
             <h2>Sneaky Strikers</h2>
-            <Button id="loginButton" onClick={this.loginProcedure}><Icon name='google' />Log In with Google</Button>
+            <Link to="/game"><Button id="startButton"><Icon name='game' />Start a game</Button></Link>
             <Link to="/leaderboard"><Button id="leaderboardButton"><Icon name='chess king' />Leaderboard</Button></Link>
+            <Button onClick={this.openModal}><Icon name='log out' />Log out</Button>
+
+            <Modal
+              open={this.state.showLogoutModal}
+              onClose={this.closeModal}
+              closeIcon
+            >
+              <Modal.Header>Log out</Modal.Header>
+              <Modal.Content>
+                <p>Are you sure you want to log out?</p>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button color='green' onClick={this.logoutProcedure}>
+                  <Icon name='checkmark' /> Yes
+                </Button>
+                <Button color='red' onClick={this.closeModal}>
+                  <Icon name='remove' /> No, do not log out
+                </Button>
+              </Modal.Actions>
+            </Modal>
           </center>
           <div class="Flex">
           </div>
@@ -135,4 +172,18 @@ export default class Home extends React.Component {
       </div>
     );
   }
+  return (
+    <div class="homePage">
+      <div class="Content">
+        <center>
+          <h2>Sneaky Strikers</h2>
+          <Button id="loginButton" onClick={this.loginProcedure}><Icon name='google' />Log In with Google</Button>
+          <Link to="/leaderboard"><Button id="leaderboardButton"><Icon name='chess king' />Leaderboard</Button></Link>
+        </center>
+        <div class="Flex">
+        </div>
+      </div>
+    </div>
+  );
+}
 };
